7eb08e4e980d481de42fa00be57ef82e040f1ed1
Caleb Xu <caleb@caleb.local>
2014-03-17 16:24:27 -0400

    Update Brew-detox

---
+++ brew-detox.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class BrewHangover < Formula
+  homepage "https://github.com/alebcay/detox"
+  head "https://github.com/alebcay/detox.git"
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'brew-detox.rb'
+    (bin+'brew-detox.rb').chmod 0755
+  end
+end

08b582480981812418d0d8a7fc3cb9fc22f66286
Caleb Xu <caleb@caleb.local>
2014-03-17 16:31:10 -0400

    Add version for Detox

--- brew-detox.rb
+++ brew-detox.rb
@@ -3,7 +3,7 @@ require "formula"
 class BrewDetox < Formula
   homepage "https://github.com/alebcay/detox"
   url "https://github.com/alebcay/homebrew-brewtap/archive/master.zip"
-
+  version "latest"
   skip_clean 'bin'
 
   def install

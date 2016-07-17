1b223ee505f171b7bc289465c43da60d664f7047
Caleb Xu <calebcenter@live.com>
2016-07-17 19:28:25 -0400

    Update histo

--- histo.rb
+++ histo.rb
@@ -3,11 +3,12 @@ require "formula"
 class Histo < Formula
   homepage "https://github.com/visionmedia/histo"
   url "https://github.com/visionmedia/histo/archive/0.0.2.tar.gz"
-  sha1 "0eec89b51716cb8a91cf9088590909fdf340662c"
+  sha256 "0794ecc9ed1c9baf1462078d3410162b35d0a0f12858ba32dde69fa3f8a7ce4e"
   head "https://github.com/visionmedia/histo.git"
 
   def install
-    system "make install"
+    system "make"
+    bin.install "histo"
   end
     
   test do

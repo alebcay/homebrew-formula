ec169715cc4de1da912ac7ad6aeddb650ee6303e
Caleb Xu <calebcenter@live.com>
2026-08-10 16:27:35 -0400

    histo: add license

--- Formula/histo.rb
+++ Formula/histo.rb
@@ -3,6 +3,7 @@ class Histo < Formula
   homepage "https://github.com/visionmedia/histo"
   url "https://github.com/tj/histo/archive/refs/tags/0.0.2.tar.gz"
   sha256 "0794ecc9ed1c9baf1462078d3410162b35d0a0f12858ba32dde69fa3f8a7ce4e"
+  license "MIT"
   head "https://github.com/visionmedia/histo.git"
 
   def install
